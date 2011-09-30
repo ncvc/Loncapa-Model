570f419de73a2e481e93646eaf78b41af25e1244
Nathan V-C <nvcarski@gmail.com>
2011-09-30 19:26:29 -0400

    Added some necessary classes

---
+++ Data.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 30 19:21:56 2011
+
+@author: Nathan
+"""
+
+class Problem:
+    def __init__(self, name, diff, tags, descr='None"):
+        self.name = name
+        self.diff = diff
+        self.tags = tags
+        self.descr = descr
+
+class PSet:
+    def __init__(self, problems):
+        self.probs = problems
+
+class Hierarchy:
+    def __init__(self):
+        pass
---
+++ Server.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 30 19:18:49 2011
+
+@author: Nathan
+"""
+
+class Server:
+    def __init__(self, name, bank):
+        self.name = name
+        self.bank = bank
