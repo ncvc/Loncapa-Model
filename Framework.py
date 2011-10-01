e299d0377dd137b2d6ff1dc8031d1f1c410c115a
Nathan V-C <nvcarski@gmail.com>
2011-09-30 21:37:42 -0400

    Very rudimentary implemetation - first working-ish commit

--- Framework.py
+++ Framework.py
@@ -5,11 +5,11 @@ Created on Fri Sep 30 19:49:15 2011
 @author: Nathan
 """
 
-from Server import Server, Bank
+from NetworkServer import Server, Bank
 
 class Framework:
     def __init__(self, numServers):
-        self.servers = dict([('Server%i', Server('Server%i' % i, Bank(), self)) for i in xrange(numServers)])
+        self.servers = dict([('Server%i' % i, Server('Server%i' % i, Bank(5), self)) for i in xrange(numServers)])
         self.loggedIn = None
     
     def sendReq(self, name, req):
@@ -20,10 +20,11 @@ class Framework:
         return [self.sendReq(server, req) for server in self.servers]
     
     def mainLoop(self):
-        inp = ''
+        inp = ['']
         
         while inp[0] != 'exit':
-            inp = raw_input('>').split()
+            rawInp = raw_input('>')
+            inp = rawInp.split()
             
             if inp[0] == 'list' and inp[1] == 'servers':
                 for name in self.servers:
@@ -36,11 +37,12 @@ class Framework:
                 print 'logged in to %s' % self.loggedIn
             else:
                 if self.loggedIn in self.servers:
-                    self.servers[self.loggedIn].command(inp)
+                    self.servers[self.loggedIn].command(rawInp)
         
         print 'Exiting'
                 
 
 if __name__ == '__main__':
     framework = Framework(5)
+    print framework.servers
     framework.mainLoop()
